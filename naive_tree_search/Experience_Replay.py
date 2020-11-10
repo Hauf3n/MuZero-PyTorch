b49bbe141176c00bbfffd680297d405129bab7c2
Hauf3n <53398268+Hauf3n@users.noreply.github.com>
2020-11-10 20:28:05 +0100

    naive tree search complezed

--- naive_tree_search/Experience_Replay.py
+++ naive_tree_search/Experience_Replay.py
@@ -60,7 +60,6 @@ class Experience_Replay():
             # add discounted rewards until step n or end of episode
             last_valid_index = np.minimum(traj_last_index, n_index)
             for i, reward in enumerate(self.memory[memory_index]["rewards"][step:last_valid_index]):
-            #for i, reward in enumerate(self.memory[memory_index]["rewards"][step::]): # rewards until end of episode
                 value += reward * (gamma ** i)
                 
             sample["return"].append(value)
@@ -88,7 +87,7 @@ class Experience_Replay():
         
         return sample
         
-    def get(self, batch_size, k, n, gamma=0.95):
+    def get(self, batch_size, k, n, gamma=0.99):
         
         data = []
         
